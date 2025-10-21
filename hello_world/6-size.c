1fe4e614b80203638cf155785a4fe9c201208c83
Alistair31 <gabikill@hotmail.fr>
2025-10-21 20:32:40 +0200

    fix: missing the librairy for m32 test

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %ld\n" ,sizeof(char));
-	printf("Size of an int: %ld\n" ,sizeof(int));
-	printf("Size of a long int: %ld\n" ,sizeof(long int));
-	printf("Size of a long long int: %ld\n" ,sizeof(long long int));
-	printf("Size of a float: %ld\n" ,sizeof(float));
+	printf("Size of a char: %ld\n", sizeof(char));
+	printf("Size of an int: %ld\n", sizeof(int));
+	printf("Size of a long int: %ld\n", sizeof(long int));
+	printf("Size of a long long int: %ld\n", sizeof(long long int));
+	printf("Size of a float: %ld\n", sizeof(float));
 
-    return (0);
+return (0);
 }
