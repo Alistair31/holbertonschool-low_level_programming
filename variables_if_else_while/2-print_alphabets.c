d8ac239c2fbb57135604a539bc76f219fe3a2eb3
Alistair31 <gabikill@hotmail.fr>
2025-10-22 14:56:38 +0200

    feat: print the alphabet in lowercase with a specific function

---
+++ variables_if_else_while/2-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alphabet;
+	for (alphabet = 97; alphabet < 123; alphabet++)
+	{
+		putchar(alphabet);
+	}
+		putchar('\n');
+	return (0);
+}
