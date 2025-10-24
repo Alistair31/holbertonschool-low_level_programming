6bab571eb452c6f03c15a7331cd52d0167524786
Alistair31 <gabikill@hotmail.fr>
2025-10-24 12:45:00 +0200

    feat: print the alphabet in lowercase and update main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 97; c < 123; c++)
+
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
