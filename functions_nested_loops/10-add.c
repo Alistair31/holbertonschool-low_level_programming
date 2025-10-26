7b08826e22986ac8aa1d4c8984045e3ee0dae85e
Alistair31 <gabikill@hotmail.fr>
2025-10-26 22:58:07 +0100

    feat: function that add two valor

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check the code
+ * @a: first variable of add
+ * @b: second variable of add
+ * return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
