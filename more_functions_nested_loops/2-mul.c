8a2d19aca1ed6ac6faba5e230ad5b2b2bde6dd29
Alistair31 <gabikill@hotmail.fr>
2025-10-27 10:37:15 +0100

    feat: function that multiplies two numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: variable that contain a number
+ * @b: variable that contain another number
+ * Return: result of the function
+ */
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
