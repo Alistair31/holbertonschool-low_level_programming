cd27591e7d66a78dc239f8d4db9ef6f4466abd62
Alistair31 <gabikill@hotmail.fr>
2025-10-29 00:26:22 +0100

    fix: comment descritpion issue, unwanted space removed

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
- /**
- * print_numbers- function used to print all digits
- * Return: Always 0 (Success)
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: void
  */
 void print_numbers(void)
 {
