398f0d26830998b64fb1701c6266ca38b6a00ba0
Alistair31 <gabikill@hotmail.fr>
2025-10-29 00:48:27 +0100

    fix: unecessary space and trailing whitespace removed

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,22 @@
 #include "main.h"
- /**
+/**
  * print_line - function used to print a straight line
  * @n: number of underscore to print
  */
 void print_line(int n)
 {
 	int ln;
-	
-		if (n <= 0)
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (ln = 0; ln < n; ln++)
 		{
-			_putchar('\n');
+			_putchar('_');
 		}
-		else
-		{
-			for (ln = 0; ln < n; ln++)
-			{
-				_putchar('_');
-			}
 			_putchar('\n');
-		}
-	
+	}
 }
