4759516b6bb84ae9bf6849978a2ef9712f221022
Alistair31 <gabikill@hotmail.fr>
2025-10-29 00:51:59 +0100

    fix: unecessary space and excess parameter removed

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,29 @@
 #include "main.h"
- /**
+/**
  * print_diagonal - function that print characters in a diagonal form
  * @n: number of line to print
- * @ln: define 
- * @sp: incredentation of space in each printed line
+ * Return: void
  */
 void print_diagonal(int n)
 {
 	int sp;
 	int ln;
-		if (n <= 0)
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (ln = 0; ln < n; ln++)
 		{
+			if (ln > 0)
+			{
+				for (sp = 0; sp < ln; sp++)
+				_putchar(' ');
+			}
+			_putchar('\\');
 			_putchar('\n');
 		}
-		else
-		{
-			for (ln = 0; ln < n; ln++)
-			{
-				if (ln > 0)
-				{
-					for (sp = 0; sp < ln; sp++)
-					_putchar(' ');
-				}
-				_putchar('\\');
-				_putchar('\n');
-			}
-		}
+	}
 }
