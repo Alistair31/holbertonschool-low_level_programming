cc9d7bf8ee3e0ef8e41108a3e9f1177cf1ba85cc
Alistair31 <gabikill@hotmail.fr>
2025-10-30 15:24:49 +0100

    feat: print only the second half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+/**
+ * puts_half - print only half of a string
+ * @str: string of characters
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int l;
+	int j;
+
+	l = _strlen(str);
+	j =((l + 1) / 2);
+
+	while (j < l)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
