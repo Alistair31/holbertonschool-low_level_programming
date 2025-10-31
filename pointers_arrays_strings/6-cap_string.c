1cb52e2def49a2d18ae2b6b7a695bfcd2fdc66d2
Alistair31 <gabikill@hotmail.fr>
2025-10-31 22:54:33 +0000

    feat: put uppercase on first letter on each word

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @sc: pointer to the string
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *sc)
+{
+	int i = 0;
+	int up = 1;
+
+	while (sc[i] != '\0')
+	{
+		if (up && sc[i] >= 97 && sc[i] <= 122)
+		{
+			sc[i] = sc[i] - 32;
+			up = 0;
+		}
+		else if (sc[i] == 32 || sc[i] == 33 || sc[i] == 34 || sc[i] == 40 || sc[i] == 41 || sc[i] == 44 || sc[i] == 46 || sc[i] == 59 || sc[i] == 63 || sc[i] == 123 || sc[i] == 125)
+		{
+			up = 1;
+		}
+		else
+		{
+			up = 0;
+		}
+		i++;
+	}
+	return (sc);
+}
