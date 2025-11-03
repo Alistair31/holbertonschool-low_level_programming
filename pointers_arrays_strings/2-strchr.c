c1221ca015cdae2ab709a8e354d301dc93508510
Alistair31 <gabikill@hotmail.fr>
2025-11-03 13:47:54 +0100

    feat: locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locate a character in a string
+ * @s: string of character
+ * @c: occurence of the character to locate
+ * Return: character in the string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	s++;
+	}
+	return ('\0');
+}
