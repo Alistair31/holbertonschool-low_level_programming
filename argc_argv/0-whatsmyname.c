d56eb2ecc023b4c47ee8df0cec35e6bdc3e66e82
Alistair31 <gabikill@hotmail.fr>
2025-11-06 18:33:53 +0100

    feat: program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - check the code
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
