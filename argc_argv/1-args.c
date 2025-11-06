fe9c5832997e76396b094223d92b3969a430735d
Alistair31 <gabikill@hotmail.fr>
2025-11-06 18:48:42 +0100

    feat:  program that prints the number of arguments passed into it

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: number of argument
+ * @argv: array of argument
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
