6f26176884f48ebb37abb6374ff428fbab5ec00c
Alistair31 <gabikill@hotmail.fr>
2025-11-12 02:34:08 +0100

    fix: pointers have to be checked separately

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,9 +18,12 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = 0;
 	len2 = 0;
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	while (s1[len1] != '\0')
