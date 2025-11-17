63b6aad574625b059752edb2e8b85854ec329e6d
Alistair31 <gabikill@hotmail.fr>
2025-11-17 14:09:00 +0100

    feat: function that executes a function given as a parameter

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: array of parameter
+ * @size: size of the array
+ * @action: fnuction to call
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action != NULL || array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
