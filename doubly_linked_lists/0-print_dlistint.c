7334b237cd653b44d3938f44153c159d5f210df3
Alistair31 <gabikill@hotmail.fr>
2025-12-03 09:17:34 +0100

    feat: function that prints all the elements of a list.

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a doubly linked list.
+ * @h: pointer to the beginning of the list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
