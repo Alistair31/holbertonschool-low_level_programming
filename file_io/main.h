16237e8397a14fa2208bc5c067c628c4428d1644
Alistair31 <gabikill@hotmail.fr>
2025-12-05 21:37:41 +0100

    feat: add the file creator function to library

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -11,5 +12,6 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
